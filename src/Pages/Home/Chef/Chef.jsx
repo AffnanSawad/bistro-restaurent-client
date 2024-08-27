3b4e852146b1ccdc6f766f6dde3f250d2edf7edc
Affnan Sawad <affnansawad2002@gmail.com>
2024-08-27 14:44:52 +0600

    Banner Added

---
+++ src/Pages/Home/Chef/Chef.jsx
@@ -0,0 +1,15 @@
+import bannerpic from "../../../assets/home/chef-service.jpg"
+
+const Chef = () => {
+    return (
+        <div>
+
+    <div className="my-8 py-4">
+        <img src={bannerpic} alt="" />
+    </div>
+
+        </div>
+    );
+};
+
+export default Chef;
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,18 @@
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+import Chef from "../Chef/Chef";
+
+
+const Home = () => {
+    return (
+        <div>
+           {/* <h1>This Is Home</h1>  */}
+
+           <Banner></Banner>
+           <Category></Category>
+           <Chef></Chef>
+        </div>
+    );
+};
+
+export default Home;
